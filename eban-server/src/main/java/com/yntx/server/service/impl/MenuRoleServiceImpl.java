760dac10857c88fe3ce0b8cfa92aac60bcd0a32c
yntx-github <172389114@qq.com>
2022-01-17 23:24:46 +0800

    提交项目结构

---
+++ eban-server/src/main/java/com/yntx/server/service/impl/MenuRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.yntx.server.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yntx.server.mapper.MenuRoleMapper;
+import com.yntx.server.pojo.MenuRole;
+import com.yntx.server.service.IMenuRoleService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author yntx
+ * @since 2022-01-17
+ */
+@Service
+public class MenuRoleServiceImpl extends ServiceImpl<MenuRoleMapper, MenuRole> implements IMenuRoleService {
+
+}
